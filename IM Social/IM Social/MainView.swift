807abf3ebede89c7672b3a9e49db9597b3bcaca7
@israman30 <you@example.com>
2023-09-22 21:07:34 -0400

    added message bubble plus input field text

--- IM Social/IM Social/MainView.swift
+++ IM Social/IM Social/MainView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainView: View {
     
     @State var inputText = ""
-    @State var messages = [String]()
+    @State var messages = ["test 1", "test 2"]
     
     var body: some View {
         NavigationView {
@@ -18,14 +18,36 @@ struct MainView: View {
                 List {
                     ForEach(messages, id: \.self) { item in
                         Text("\(item)")
+                            .padding()
+                            .font(.title3)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(15)
                     }
                 }
-                TextField("Say something..", text: $inputText)
-                    .padding()
+                .listStyle(.grouped)
+                HStack {
+                    TextField("Say something..", text: $inputText)
+                        .padding()
                     
+                    Button {
+                        // send
+                        self.addNewMessage(message: inputText)
+                    } label: {
+                        Image(systemName: "arrow.uturn.backward")
+                            .padding()
+                    }
+                    .buttonStyle(.bordered)
+
+                }
             }
             .navigationTitle("IM Social")
         }
+    }
+    
+    func addNewMessage(message: String) {
+        messages.append(message)
+        inputText = ""
     }
 }
 
