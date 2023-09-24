c7e449a51915643c9fd987a5710cd8212f81df53
@israman30 <you@example.com>
2023-09-23 20:26:30 -0400

    added message section for sender and receiver

--- IM Social/IM Social/MessageView.swift
+++ IM Social/IM Social/MessageView.swift
@@ -12,17 +12,42 @@ struct MessageView: View {
     var object: Message
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("07/12/2023")
-                .font(.caption)
-                .padding(.horizontal, 5)
-            Text("\(object.message)")
-                .padding()
-                .font(.title3)
-                .fontWeight(.medium)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(20)
+        if object.sendeId == "abc" {
+            HStack {
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("07/12/2023")
+                        .font(.caption)
+                        .padding(.horizontal, 5)
+                    Text("\(object.message)")
+                        .padding()
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                }
+            }
+            .padding()
+            
+        } else {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("07/12/2023")
+                        .font(.caption)
+                        .padding(.horizontal, 5)
+                    Text("\(object.message)")
+                        .padding()
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .background(Color(.systemGray2))
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                }
+                Spacer()
+            }
+            .padding()
+            
         }
         
     }
